0c68fb819e8fcb671f888fc9c39e89b02f840689
Upendra Sahu <upendrasahu1199@gmail.com>
2025-01-22 00:57:43 +0530

    Add subject list in subjects page

--- src/pages/Subjects.tsx
+++ src/pages/Subjects.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Subjects = () => {
+  const subjects = [
+    "mathematics",
+    "reasoning",
+    "history",
+    "geography",
+    "science",
+    "hindi",
+    "english",
+    "economy",
+    "currentaffairs",
+    "computers"
+  ];
+  
   return (
-    <div>Subjects</div>
+    <ul className="menu menu-md bg-base-200 rounded-box w-56 mx-auto">
+      {
+        subjects.map((subject, index)=>
+            <li key={index}><Link to={`/subjects/${subject}`}>{subject}</Link></li>
+        )
+      }
+    </ul>
   )
 }
 
