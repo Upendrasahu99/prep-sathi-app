62c2a21c69c3c283ce14624b142c303f521202c9
Upendra Sahu <upendrasahu1199@gmail.com>
2025-01-26 12:44:32 +0530

    Update for select time using html select

--- src/components/testSetting/TestSetting.tsx
+++ src/components/testSetting/TestSetting.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
 const TestSetting = () => {
-
+  const hours = [1, 2, 3];
+  const minutes = [];
+  const seconds = [];
+  for(let i = 0; i < 60; i++){
+    minutes.push(i);
+    seconds.push(i);
+  }
   return (
     <div>
-      <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-      <input type="time" step={1} placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+      <div>
+        <p>Select duration</p>
+        <select className="select select-bordered " defaultValue='hour'>
+          <option disabled value='hour'>hour</option>
+          {
+            hours.map((data, index) => <option key={index} value={data}>{data}</option>)
+          }
+        </select>
+        <select className="select select-bordered " defaultValue='min'>
+          <option disabled value='min'>min</option>
+          {
+            minutes.map((data, index) => <option key={index} value={data}>{data}</option>)
+          }
+        </select>
+        <select className="select select-bordered " defaultValue='sec'>
+          <option disabled value='sec'>sec</option>
+          {
+            seconds.map((data, index) => <option key={index} value={data}>{data}</option>)
+          }
+        </select>
+      </div>
+
     </div>
   )
 }
