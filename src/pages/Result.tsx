7b288d234ce048b6ec3b45b13190a0696829e4d4
Upendra Sahu <upendrasahu1199@gmail.com>
2025-03-03 01:14:22 +0530

    Modify show time taken for question restul card

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -114,6 +114,15 @@ const Result: React.FC = () => {
               }`}
             >
               <div className="card-body">
+                <div className="mt-2 p-2 bg-base-200 rounded-lg shadow-sm flex items-center gap-2">
+                  <svg className="w-5 h-5 text-gray-600 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <div className="badge badge-neutral rounded-lg text-xl">
+                  <span className="font-semibold text-gray-900 dark:text-gray-100">Time Taken: </span>
+                  <span className="ms-1 font-extrabold"> {formatTime(question.timeTaken)}</span>
+                </div>
+                </div>
                 <div>
                   <div className="badge badge-neutral">{qIndex + 1}.</div>{' '}
                   <span className="text-gray-900 dark:text-gray-100">{question.questionHindi}</span>
@@ -121,10 +130,6 @@ const Result: React.FC = () => {
                 <div>
                   <div className="badge badge-neutral">{qIndex + 1}.</div>{' '}
                   <span className="text-gray-900 dark:text-gray-100">{question.questionEnglish}</span>
-                </div>
-                <div className="mt-2">
-                  <span className="font-semibold text-gray-900 dark:text-gray-100">Time Taken: </span>
-                  <span className="text-gray-600 dark:text-gray-400">{formatTime(question.timeTaken)}</span>
                 </div>
                 <div className="flex flex-col gap-5 mt-5">
                   {question.questionOptionsHindi.map((option: any, index: number) => {
